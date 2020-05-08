be9c67e9bf52e91980d913393ecadbc6e5ca1fac
AkIonSight <akionsight@gmail.com>
2020-05-08 10:09:49 +0530

    homework management system
    This is a small homework management system. has Three commands ---- add:- to add homework, list:- to list homework, del:- delete homeworks with security so that you do not delete it accidently

---
+++ homework_management.py
@@ -0,0 +1,32 @@
+def add_homework():
+    homework = input("add homework:- ")
+    file = open('play.txt', 'a')
+    file.write(f'{homework}\n')
+
+def list_homework():
+    file = open('play.txt', 'r+') 
+    read = file.read()
+    print(read)
+def turncate():
+    import random
+    num = random.randint(100000,999999)
+    file = open("play.txt", 'w+')
+    print(num)
+    ent = input("enter the number shown above that you want to delete your homework:- ")
+    if ent == num:
+        file.truncate(0)
+
+
+
+
+input_ = input(">>> ")
+
+if input_ == 'add':
+    add_homework()
+    print("homework added")
+elif input_ == 'list':
+    list_homework()
+elif input_ == 'del':
+    turncate()
+    print("contents of file deleted")
+    
